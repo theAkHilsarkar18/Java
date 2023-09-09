8a49e6bbe82edf49af9f85fc61e5901c1b9a93db
theakhilsarkar18 <akhilsodvadiya7@gmail.com>
2023-09-09 23:36:08 +0530

    09/09/2023

--- src/Precedence.java
+++ src/Precedence.java
@@ -1,6 +1,17 @@
 public class Precedence {
     public static void main(String[] args)
     {
+        int x = 4, y = 2, z = 3;
+        int ans;
+        ans = ++x*z/y; // 7
+        System.out.println(ans);
+        ans = x+y-z*x/x;
+        // approach - 1
+        // z*x = 12
+        // 12/x = 4
+        // 4+x = 8
+        // 8-z = 5
+        System.out.println(ans);
 
     }
 }
